1378c94cf83045f43c6880336bc5b38347800e21
Pasan Abeysekara <pasankavindaabey@gmail.com>
2024-03-27 03:59:28 +0530

    Update 1.0: configure environment and github actions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dirprintly',
-    version='0.3',  # Ensure this version number is incremented if re-uploading.
+    version='1.0',  # Ensure this version number is incremented if re-uploading.
     packages=find_packages(),
     install_requires=[
         'colorama',
